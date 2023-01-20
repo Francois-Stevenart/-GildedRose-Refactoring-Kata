65b44a97903ea92908803cf48a1a0069552608c7
François Stévenart <f.stevenart@gmail.com>
2023-01-20 14:46:33 +0100

    Start refactoring: add pseudo-code and improve readability by storing iterated item as a constant

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,47 +18,70 @@ export class GildedRose {
   }
 
   updateQuality() {
+    // For each item...
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+      const currentItem = this.items[i];
+      // If not special Aged Brie or Backstage:
+      if (currentItem.name != 'Aged Brie' && currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        // ...and if quality above 0...
+        if (currentItem.quality > 0) {
+          // ...and if not special Sulfuras
+          if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
+            // => then lower Quality by 1.
+            currentItem.quality = currentItem.quality - 1
           }
         }
+      // Else (if special product):
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+        // if Quality under 50...
+        if (currentItem.quality < 50) {
+          // => then increase Quality by 1.
+          currentItem.quality = currentItem.quality + 1
+          // If special Backstage:
+          if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
+            // If sellIn under 11 days and Quality under 50:
+            if (currentItem.sellIn < 11) {
+              if (currentItem.quality < 50) {
+                // => then increase Quality by 1 more:
+                currentItem.quality = currentItem.quality + 1
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
+            // If sellIn under 6 days and Quality under 50:
+            if (currentItem.sellIn < 6) {
+              if (currentItem.quality < 50) {
+                // => then increase Quality by 1 more:
+                currentItem.quality = currentItem.quality + 1
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      // For all items except legendary Sulfuras, decrease sellIn by 1
+      if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
+        currentItem.sellIn = currentItem.sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+      // For all items whose sellIn is under 0....
+      if (currentItem.sellIn < 0) {
+        //-- If it's not an Aged Brie...:
+        if (currentItem.name != 'Aged Brie') {
+          // ...if it's not a Backstage....
+          if (currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            // ...if quality is above 0 and it's not legendary Sulfuras....
+            if (currentItem.quality > 0) {
+              if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
+                // ... then lower Quality by 1 extra unit
+                currentItem.quality = currentItem.quality - 1
               }
             }
+            // ...else, if it's Backstage, set Quality to 0
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            currentItem.quality = currentItem.quality - currentItem.quality
           }
+        // -- If it's an Aged Brie:
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
+          // As long as quality is under 50, increase Quality by 1 extra unit
+          if (currentItem.quality < 50) {
+            currentItem.quality = currentItem.quality + 1
           }
         }
       }
