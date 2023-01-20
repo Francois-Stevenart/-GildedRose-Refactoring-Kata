49c47f77a17cbc6b5202153b53cc4a3fe0dd105f
François Stévenart <f.stevenart@gmail.com>
2023-01-20 15:09:26 +0100

    Reduce nested if statements by combining conditions and reducing implicit else by explicit if

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,37 +21,28 @@ export class GildedRose {
     // For each item...
     for (let i = 0; i < this.items.length; i++) {
       const currentItem = this.items[i];
-      // If not special Aged Brie or Backstage:
-      if (currentItem.name != 'Aged Brie' && currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        // ...and if quality above 0...
-        if (currentItem.quality > 0) {
-          // ...and if not special Sulfuras
-          if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
-            // => then lower Quality by 1.
-            currentItem.quality = currentItem.quality - 1
-          }
-        }
+      // If quality above 0...
+      // ...and if not special Aged Brie or Backstage or Sulfuras
+      // => then lower Quality by 1.
+      if (currentItem.quality > 0 && !['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(currentItem.name)) {
+        currentItem.quality = currentItem.quality - 1
       // Else (if special product):
       } else {
         // if Quality under 50...
+        // => then increase Quality by 1.
         if (currentItem.quality < 50) {
-          // => then increase Quality by 1.
           currentItem.quality = currentItem.quality + 1
-          // If special Backstage:
-          if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            // If sellIn under 11 days and Quality under 50:
-            if (currentItem.sellIn < 11) {
-              if (currentItem.quality < 50) {
-                // => then increase Quality by 1 more:
-                currentItem.quality = currentItem.quality + 1
-              }
-            }
-            // If sellIn under 6 days and Quality under 50:
-            if (currentItem.sellIn < 6) {
-              if (currentItem.quality < 50) {
-                // => then increase Quality by 1 more:
-                currentItem.quality = currentItem.quality + 1
-              }
+        // If special Backstage:
+        if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          // If sellIn under 11 days and Quality under 50:
+          // => then increase Quality by 1 more:
+          if (currentItem.quality < 50 && currentItem.sellIn < 11) {
+            currentItem.quality = currentItem.quality + 1
+          }
+          // If sellIn under 6 days and Quality under 50:
+          // => then increase Quality by 1 more:
+          if (currentItem.quality < 50 && currentItem.sellIn < 6) {
+            currentItem.quality = currentItem.quality + 1
             }
           }
         }
@@ -62,27 +53,19 @@ export class GildedRose {
       }
       // For all items whose sellIn is under 0....
       if (currentItem.sellIn < 0) {
-        //-- If it's not an Aged Brie...:
-        if (currentItem.name != 'Aged Brie') {
-          // ...if it's not a Backstage....
-          if (currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            // ...if quality is above 0 and it's not legendary Sulfuras....
-            if (currentItem.quality > 0) {
-              if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
-                // ... then lower Quality by 1 extra unit
-                currentItem.quality = currentItem.quality - 1
-              }
-            }
-            // ...else, if it's Backstage, set Quality to 0
-          } else {
-            currentItem.quality = currentItem.quality - currentItem.quality
-          }
-        // -- If it's an Aged Brie:
-        } else {
-          // As long as quality is under 50, increase Quality by 1 extra unit
-          if (currentItem.quality < 50) {
-            currentItem.quality = currentItem.quality + 1
-          }
+        // If quality above 0...
+        // ...and if not special Aged Brie or Backstage or Sulfuras
+        // => then then lower Quality by 1 extra unit
+        if (currentItem.quality > 0 && !['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(currentItem.name)) {
+          currentItem.quality = currentItem.quality - 1
+        }
+        // If it's Backstage:
+        if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          currentItem.quality = currentItem.quality - currentItem.quality
+        }
+        // If it's Aged Brie and quality under 50
+        if (currentItem.name == 'Aged Brie' && currentItem.quality < 50) {
+          currentItem.quality = currentItem.quality + 1
         }
       }
     }
