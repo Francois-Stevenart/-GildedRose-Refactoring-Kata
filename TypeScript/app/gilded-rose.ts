24c885026ca10651c811d18ed8db7e8bceebe616
François Stévenart <f.stevenart@gmail.com>
2023-01-20 16:20:11 +0100

    First big refactoring: main switch statement and redundant logic encapsulated in reusable class method helpers

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -12,62 +12,59 @@ export class Item {
 
 export class GildedRose {
   items: Array<Item>;
+  specialProducts: string[];
+  maxQuality: number;
+  minQuality: number;
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
+    this.specialProducts = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'];
+    this.maxQuality = 50;
+    this.minQuality = 0;
+  }
+
+  private setItemQuality(item: Item, amount: number) {
+    if (!amount) return;
+
+    const updatedQuality = item.quality + amount
+
+    item.quality = updatedQuality >= this.minQuality
+      ? Math.min(updatedQuality, this.maxQuality)
+      : Math.max(updatedQuality, this.minQuality)
+  }
+
+  private setItemSellIn(item: Item, amount: number) {
+    item.sellIn = item.sellIn + amount;
   }
 
   updateQuality() {
-    // For each item...
     for (let i = 0; i < this.items.length; i++) {
       const currentItem = this.items[i];
-      // If quality above 0...
-      // ...and if not special Aged Brie or Backstage or Sulfuras
-      // => then lower Quality by 1.
-      if (currentItem.quality > 0 && !['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(currentItem.name)) {
-        currentItem.quality = currentItem.quality - 1
-      // Else (if special product):
-      } else {
-        // if Quality under 50...
-        // => then increase Quality by 1.
-        if (currentItem.quality < 50) {
-          currentItem.quality = currentItem.quality + 1
-        // If special Backstage:
-        if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          // If sellIn under 11 days and Quality under 50:
-          // => then increase Quality by 1 more:
-          if (currentItem.quality < 50 && currentItem.sellIn < 11) {
-            currentItem.quality = currentItem.quality + 1
+
+      let qualityUpdate: number
+      let sellInUpdate = -1;
+
+      switch (currentItem.name) {
+        case ('Aged Brie'):
+          qualityUpdate = currentItem.sellIn <= 0 ? 2 : 1;
+          break;
+        case ('Backstage passes to a TAFKAL80ETC concert'):
+          if (currentItem.sellIn <= 0) {
+            qualityUpdate = -currentItem.quality
+          } else {
+            qualityUpdate = currentItem.sellIn < 6 && 3 || currentItem.sellIn < 11 && 2 || 1
           }
-          // If sellIn under 6 days and Quality under 50:
-          // => then increase Quality by 1 more:
-          if (currentItem.quality < 50 && currentItem.sellIn < 6) {
-            currentItem.quality = currentItem.quality + 1
-            }
-          }
-        }
+          break;
+        case ('Sulfuras, Hand of Ragnaros'):
+          qualityUpdate = 0;
+          sellInUpdate = 0;
+          break;
+        default:
+          qualityUpdate = currentItem.sellIn <= 0 ? -2 : -1;
       }
-      // For all items except legendary Sulfuras, decrease sellIn by 1
-      if (currentItem.name != 'Sulfuras, Hand of Ragnaros') {
-        currentItem.sellIn = currentItem.sellIn - 1;
-      }
-      // For all items whose sellIn is under 0....
-      if (currentItem.sellIn < 0) {
-        // If quality above 0...
-        // ...and if not special Aged Brie or Backstage or Sulfuras
-        // => then then lower Quality by 1 extra unit
-        if (currentItem.quality > 0 && !['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(currentItem.name)) {
-          currentItem.quality = currentItem.quality - 1
-        }
-        // If it's Backstage:
-        if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          currentItem.quality = currentItem.quality - currentItem.quality
-        }
-        // If it's Aged Brie and quality under 50
-        if (currentItem.name == 'Aged Brie' && currentItem.quality < 50) {
-          currentItem.quality = currentItem.quality + 1
-        }
-      }
+
+      this.setItemQuality(currentItem, qualityUpdate);
+      this.setItemSellIn(currentItem, sellInUpdate);
     }
 
     return this.items;
