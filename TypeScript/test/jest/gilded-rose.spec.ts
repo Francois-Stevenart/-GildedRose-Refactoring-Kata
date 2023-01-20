8f2a5e2781ebd6d875bb8d7d8ee51473566a52f0
François Stévenart <f.stevenart@gmail.com>
2023-01-20 12:51:04 +0100

    Base testing & structure for standard behaviour scenario

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,32 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+function range(n) {
+  return [...Array(n).keys()]
+}
+
+describe('Gilded Rose system for standard products', () => {
+  let gildedRose
+
+  beforeEach(() => {
+    const standardItem = new Item('Sausages', 5, 12)
+    gildedRose = new GildedRose([standardItem]);
+  })
+
+  test('should lower the SellIn value by 1 each day', () => {
+    gildedRose.updateQuality();
+    const updatedStandardItem = gildedRose.items[0];
+
+    expect(updatedStandardItem.name).toBe('Sausages');
+    expect(updatedStandardItem.sellIn).toBe(4);
+    expect(updatedStandardItem.quality).toBe(11);
   });
+
+  test('should lower the SellIn value linearly after multiple days', () => {
+    range(4).forEach(day => gildedRose.updateQuality());
+    const updatedStandardItem = gildedRose.items[0];
+
+    expect(updatedStandardItem.name).toBe('Sausages');
+    expect(updatedStandardItem.sellIn).toBe(1);
+    expect(updatedStandardItem.quality).toBe(8);
+  })
 });
