be7213ca36819d3bbb615fdb1e203b5a61339190
François Stévenart <f.stevenart@gmail.com>
2023-01-20 18:35:16 +0100

    Major refactor: declarative-based with config implementation for easier flexibility to add new products without needing to touch the code

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,39 @@
+interface Config {
+  minQuality: number,
+  maxQuality: number,
+  products: {
+    [productName: string]: { minSellInDay: number, maxSellInDay: number, qualityIncrement: number, sellInIncrement?: number }[]
+  }
+}
+
+const CONFIG: Config = {
+  minQuality: 0,
+  maxQuality: 50,
+  products: {
+    'Aged Brie': [
+      { minSellInDay: 0, maxSellInDay: Infinity, qualityIncrement: 1 },
+      { minSellInDay: -Infinity, maxSellInDay: 0, qualityIncrement: 2 }
+    ],
+    'Backstage passes to a TAFKAL80ETC concert': [
+      { minSellInDay: 10, maxSellInDay: Infinity, qualityIncrement: 1 },
+      { minSellInDay: 5, maxSellInDay: 10, qualityIncrement: 2 },
+      { minSellInDay: 0, maxSellInDay: 5, qualityIncrement: 3 },
+      { minSellInDay: -Infinity, maxSellInDay: 0, qualityIncrement: -Infinity }
+    ],
+    'Sulfuras, Hand of Ragnaros': [
+      { minSellInDay: -Infinity, maxSellInDay: Infinity, qualityIncrement: 0, sellInIncrement: 0 },
+    ],
+    'Conjured': [
+      { minSellInDay: 0, maxSellInDay: Infinity, qualityIncrement: -2 },
+      { minSellInDay: -Infinity, maxSellInDay: 0, qualityIncrement: -4 }
+    ],
+    default: [
+      { minSellInDay: 0, maxSellInDay: Infinity, qualityIncrement: -1 },
+      { minSellInDay: -Infinity, maxSellInDay: 0, qualityIncrement: -2 }
+    ]
+  }
+}
+
 export class Item {
   name: string;
   sellIn: number;
@@ -10,28 +46,26 @@ export class Item {
   }
 }
 
-const MAX_QUALITY = 50;
-const MIN_QUALITY = 0;
-
-enum SPECIAL_PRODUCTS {
-  agedBrie = 'Aged Brie',
-  backstagePasses = 'Backstage passes to a TAFKAL80ETC concert',
-  sulfuras = 'Sulfuras, Hand of Ragnaros',
-  conjured = 'Conjured'
-}
-
 export class GildedRose {
   items: Array<Item>;
+  config: Config;
   maxQuality: number;
   minQuality: number;
 
   constructor(items = [] as Array<Item>) {
     this.items = items;
-    this.maxQuality = MAX_QUALITY;
-    this.minQuality = MIN_QUALITY;
+    this.config = CONFIG;
+    this.maxQuality = this.config.maxQuality;
+    this.minQuality = this.config.minQuality;
   }
 
-  private setItemQuality(item: Item, amount: number) {
+  private findRelevantProductConfig(item: Item) {
+    return (this.config.products[item.name] || this.config.products['default']).find(config => {
+      return item.sellIn > config.minSellInDay && item.sellIn <= config.maxSellInDay
+    })
+  }
+
+  private setItemQuality(item: Item, amount = -1) {
     if (!amount) return;
 
     const updatedQuality = item.quality + amount
@@ -41,44 +75,17 @@ export class GildedRose {
       : Math.max(updatedQuality, this.minQuality)
   }
 
-  private setItemSellIn(item: Item, amount: number) {
+  private setItemSellIn(item: Item, amount = -1) {
     item.sellIn = item.sellIn + amount;
-  }
-
-  private isSellDatePassed(item: Item) {
-    return item.sellIn <= 0;
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const currentItem = this.items[i];
+      const relevantProductConfig = this.findRelevantProductConfig(currentItem);
 
-      let qualityUpdateValue: number;
-      let sellInUpdateValue = -1;
-
-      switch (currentItem.name) {
-        case (SPECIAL_PRODUCTS.agedBrie):
-          qualityUpdateValue = this.isSellDatePassed(currentItem) ? 2 : 1;
-          break;
-        case (SPECIAL_PRODUCTS.backstagePasses):
-          qualityUpdateValue = this.isSellDatePassed(currentItem) && -currentItem.quality ||
-                          currentItem.sellIn < 6 && 3 ||
-                          currentItem.sellIn < 11 && 2 ||
-                          1
-          break;
-        case (SPECIAL_PRODUCTS.sulfuras):
-          qualityUpdateValue = 0;
-          sellInUpdateValue = 0;
-          break;
-        case (SPECIAL_PRODUCTS.conjured):
-          qualityUpdateValue = this.isSellDatePassed(currentItem) ? -4 : -2;
-          break;
-        default:
-          qualityUpdateValue = this.isSellDatePassed(currentItem) ? -2 : -1;
-      }
-
-      this.setItemQuality(currentItem, qualityUpdateValue);
-      this.setItemSellIn(currentItem, sellInUpdateValue);
+      this.setItemQuality(currentItem, relevantProductConfig?.['qualityIncrement']);
+      this.setItemSellIn(currentItem, relevantProductConfig?.['sellInIncrement']);
     }
 
     return this.items;
